15b3e45bc74d4ad9eff86c065912cf978ca7a36a
MaB@2210 <mannbhatt.00@gmail.com>
2023-08-08 16:40:24 -0400

    Admin Pannel Created and Working.

---
+++ QuestionAnswerApp/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using QuestionAnswerApp.Models;
+
+namespace QuestionAnswerApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public UserController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+
+        public async Task<IActionResult> Manage()
+        {
+            UserViewModel uvm = new UserViewModel();
+            uvm.Users = userManager.Users.ToList();
+            uvm.Roles = roleManager.Roles.ToList();
+            foreach (IdentityUser u in uvm.Users)
+            {
+                var listOfRoles = await userManager.GetRolesAsync(u);
+                uvm.UserRoles.Add(u.UserName, listOfRoles);
+            }
+            return View(uvm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityUser usr = await userManager.FindByIdAsync(id);
+            if (usr != null)
+            {
+                await userManager.DeleteAsync(usr);
+            }
+            return RedirectToAction("Manage");
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddToAdmin(string id)
+        {
+            IdentityRole adminrole = await roleManager.FindByNameAsync("Admin");
+            IdentityUser usr = await userManager.FindByIdAsync(id);
+            await userManager.AddToRoleAsync(usr, adminrole.Name);
+            return RedirectToAction("Manage");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromAdmin(string id)
+        {
+            IdentityRole adminrole = await roleManager.FindByNameAsync("Admin");
+            IdentityUser usr = await userManager.FindByIdAsync(id);
+            await userManager.RemoveFromRoleAsync(usr, adminrole.Name);
+            return RedirectToAction("Manage");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            IdentityRole adminrole = await roleManager.FindByIdAsync(id);
+            await roleManager.DeleteAsync(adminrole);
+            return RedirectToAction("Manage");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAdminRole(string id)
+        {
+            await roleManager.CreateAsync(new IdentityRole("Admin"));
+            return RedirectToAction("Manage");
+        }
+
+    }
+}
---
+++ QuestionAnswerApp/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuestionAnswerApp.Models
+{
+    [NotMapped]
+    public class UserViewModel
+    {
+        public List<IdentityUser> Users { get; set; }
+        public List<IdentityRole> Roles { get; set; }
+
+        public Dictionary<string, IList<string>> UserRoles { get; set; } = new Dictionary<string, IList<string>>();
+    }
+}
